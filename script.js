cdd0d041f9646b9586cbfa710b26aa62e5689d0f
Winston White <whitew2015@fau.edu>
2024-11-14 14:12:49 -0500

    removed clear-button function; created regex variable to check valid numbers

--- script.js
+++ script.js
@@ -6,20 +6,29 @@ const result = document.getElementById("results-div");
 
 const validNumber = () => {
     if (input.value === "") {
-        alert("Please provide a number");
+        alert("Please provide a phone number");
         return;
     }
-}
 
+    // regex variable for valid phone numbers
+    const numberRegex = /^\d?\s?\(?\d{3}\)?[-\s]?\d{3}[-\s]?\d{4}$/g;
 
-const clearScreen = () => {
-    result.innerText = "";
+    // check regex with user input and display if the number is valid
+    if (numberRegex.test(input.value)) {
+        result.textContent = `Valid US number: ${input.value}`;
+    } else {
+        result.textContent = `Invalid US number: ${input.value}`;
+    }
 }
 
 
 checkBtn.addEventListener("click", validNumber);
 
-clearBtn.addEventListener("click", clearScreen);
+// clear input field and texts 
+clearBtn.addEventListener("click", () => {
+    result.textContent = "";
+    input.value = "";
+});
 
 input.addEventListener("keydown", e => {
     if (e.key === "Enter") {
